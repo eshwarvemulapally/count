5c69ff2ee3507a70c35862a8eebac9d8b9344379
Eshwar <eshwarchdev@gmail.com>
2017-10-23 20:50:53 -0500

    ask

--- about.js
+++ about.js
@@ -1,5 +1,19 @@
 (function() {
     var a = b = 5;
+    a = 1;
+    function test() {
+        var a;
+        function foo() {
+           return 2;
+        }
+     
+        console.log(a);
+        console.log(foo());
+        
+        a = 1;
+     }
+     
+     test();
  })();
  
  console.log('hello'.repeatify(3));
